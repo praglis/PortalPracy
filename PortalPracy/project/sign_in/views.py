3d5f320854eb0aed26544d235348eb4032959e8d
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-04-13 15:48:02 +0200

    login/logout

--- PortalPracy/project/sign_in/views.py
+++ PortalPracy/project/sign_in/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def index(request):
@@ -11,8 +12,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('offerts:index')
+            messages.success(request, f'Congratolations {username}! Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'sign_in/register.html', {'form': form})
+
+@login_required # decorator - adds functionality to an existing function
+def profile(request):
+    return render(request, 'sign_in/profile.html')
