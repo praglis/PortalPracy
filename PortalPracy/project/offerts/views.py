752f04ebb46adc06beffee3c705a5a1eebe2fbb5
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-04-14 17:10:39 +0200

    class based view, offerts on homepage displayed from newest to oldest

--- PortalPracy/project/offerts/views.py
+++ PortalPracy/project/offerts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView
 
 from .models import Offert, Company, Agency
 
-def index(request):
-    offert_list = Offert.objects.all()
-    context = {'offert_list' : offert_list}
-    return render(request, 'offerts/index.html', context )
+# class based view
+class OffertListView(ListView):
+    model = Offert
+    template_name = 'offerts/index.html' # default: <app>/<model>_<viewtype>.html
+    context_object_name = 'offerts'
+    ordering = ['-publication_date'] #from newest to oldest
+
 
 def details(request, offert_id):
     offert = get_object_or_404(Offert, pk=offert_id)
