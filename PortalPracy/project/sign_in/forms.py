d33f82770f60663d15630c5f3c0ffe1b8e5a86e5
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-04-14 15:21:47 +0200

    updating user info

--- PortalPracy/project/sign_in/forms.py
+++ PortalPracy/project/sign_in/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # form that inherits from UserCreationForm
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User # model that is going to be affected when form.save() is done
         fields = ['username', 'email', 'password1', 'password2'] # fields that we want in the form and in that order
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    # no additional fields needed so...
+    class Meta:
+        model = Profile
+        fields = ['image']
