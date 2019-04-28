096cb1c6fc46788fb64e59b08a8cdd1eb382f5ac
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-04-28 20:30:47 +0200

    Password reset mockup

--- PortalPracy/project/project/urls.py
+++ PortalPracy/project/project/urls.py
@@ -32,9 +32,22 @@ urlpatterns = [
     path('', offerts_views.home, name='home'),
     path('register/', sign_in_views.register, name ='register'),
     path('profile/', sign_in_views.profile, name ='profile'),
-    path('login/',auth_views.LoginView.as_view(template_name='sign_in/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='sign_in/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='sign_in/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='sign_in/logout.html'), name='logout'),
+    path('password-reset/',
+          auth_views.PasswordResetView.as_view(template_name='sign_in/password_reset.html'),
+          name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+          auth_views.PasswordResetConfirmView.as_view(template_name='sign_in/password_reset_confirm.html'),
+          name='password_reset_confirm'),
+    path('password-reset/done/',
+          auth_views.PasswordResetDoneView.as_view(template_name='sign_in/password_reset_done.html'),
+          name='password_reset_done'),
+    path('password-reset-complete/',
+          auth_views.PasswordResetCompleteView.as_view(template_name='sign_in/password_reset_complete.html'),
+          name='password_reset_complete'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
