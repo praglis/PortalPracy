15b1439ece29028566e2aed6a90aaf46a9518dae
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-05-11 19:47:34 +0200

    Offert deletion fixed

--- PortalPracy/project/manager/urls.py
+++ PortalPracy/project/manager/urls.py
@@ -9,6 +9,6 @@ from . import views
 app_name = 'manager'
 urlpatterns = [
     path('delete/<int:pk>/', OffertDeleteView.as_view(), name='manager-delete'),
-    path('user/<str:username>', MyOffertsListView.as_view(), name='manager-my'),
+    path('user/<str:username>/', MyOffertsListView.as_view(), name='manager-my'),
     path('update/<int:pk>/', OffertUpdateView.as_view(), name='manager-update'),
 ]
