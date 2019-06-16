82fcd3b2ad2bae999ba4cd0ca76829a005d06115
Magdalena Sawicka <saw.magdalena@protonmail.com>
2019-06-17 01:53:57 +0200

    new complete offerts

--- PortalPracy/project/search/views.py
+++ PortalPracy/project/search/views.py
@@ -42,8 +42,6 @@ def index(request):
 
     if per_hour == 'on':
         qs = qs.filter(salary_type = 'H')
-    else:
-        qs = qs.filter(salary_type = 'M')
 
     context = {
         'queryset' : qs,
